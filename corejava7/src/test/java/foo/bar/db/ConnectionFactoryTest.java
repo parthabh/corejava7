16b0a5e5d8b8dcf87054877e2619270fa8d8ef73
parthabh <parthabh@gmail.com>
2012-12-02 23:38:08 +0530

    Add contiperf. Run tests 100 times in 2 threads.

--- corejava7/src/test/java/foo/bar/db/ConnectionFactoryTest.java
+++ corejava7/src/test/java/foo/bar/db/ConnectionFactoryTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.assertNotNull;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,12 +17,16 @@ public class ConnectionFactoryTest {
 	private final static Logger logger = LoggerFactory
 			.getLogger(ConnectionFactoryTest.class);
 
+	@Rule
+	public ContiPerfRule i = new ContiPerfRule();
+
 	@Test
+	@PerfTest(invocations = 100, threads = 2)
+	@Required(max = 150, average = 100)
 	public void test() throws SQLException {
 		Connection connection = ConnectionFactory.getOracleConnection("XE",
 				"foobar", "foobar");
 		assertNotNull(connection);
 		connection.close();
-		
 	}
 }
